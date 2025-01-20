2c0cecb2814d4130fe438e8eb836b7b594fb5eac
Marianw02 <mwasiewicz@onet.eu>
2025-01-20 19:31:48 +0100

    Aplikacja desktopowa

--- Aplikacja_desktopowa.py
+++ Aplikacja_desktopowa.py
@@ -5,62 +5,80 @@ import time
 import threading
 
 ser = None
+data_previous = ''
 
-
-def send_temperature():
-    temperature = entry.get()
+def connect_with_USART():
     selected_port = port_var.get()
-
-    if not temperature.isdigit() or len(temperature) != 2:
-        messagebox.showerror("Błąd", "Wprowadź dwucyfrową liczbę.")
-        return
+    global ser
 
     try:
-        global ser
-        if ser is None or not ser.is_open:
-            ser = serial.Serial(selected_port, 9600, timeout=1)
-            time.sleep(2)
-
-        ser.write(temperature.encode())
-        messagebox.showinfo("Sukces", f"Wysłano temperaturę: {temperature}°C do {selected_port}")
+        ser = serial.Serial(selected_port, 9600, timeout=0)#polaczenie z portem
+        time.sleep(2)
+        if ser.is_open:
+            connect_with_USART_button.config(text="Rozłącz", command=disconnect_with_USART)
+            messagebox.showinfo("Sukces", f"Połączono się z portem " + selected_port)
+            data_previous = ''
 
     except serial.SerialException as e:
         messagebox.showerror("Błąd", f"Nie można otworzyć portu: {e}")
 
+def disconnect_with_USART():
+    if ser.is_open:
+        ser.close()
+        connect_with_USART_button.config(text="Połącz", command=connect_with_USART)
+        messagebox.showinfo("Sukces", f"Rozłączono się z portem szeregowym")
+
+def send_temperature():
+    temperature = entry.get()
+
+    if not temperature.isdigit() or len(temperature) != 2:
+        messagebox.showerror("Błąd", "Wprowadź wartość temperatury w zakresie od 12°C do 41°C.")
+        return
+
+    ser.write(temperature.encode())
 
 def receive_data():
+    global data_previous
+
     if ser and ser.is_open:
         if ser.in_waiting > 0:
-            data = ser.readline().decode('utf-8').strip()
-            display_received_data(data)
+            data = ser.readline().decode('utf-8')
+            for i in range(len(data)):
+                if data[i] == 'A':
+                    display_received_data(data_previous)
+                    data_previous = data[i]
+                else:
+                    data_previous = data_previous + data[i]
 
     root.after(100, receive_data)
 
 
 def display_received_data(data):
-    received_text.config(state=tk.NORMAL)
-    received_text.insert(tk.END, f"Odebrano: {data}\n")
-    received_text.config(state=tk.DISABLED)
-    received_text.see(tk.END)
+    received_text.config(state=tk.NORMAL)  #właczenie edycji
+    received_text.insert(tk.END, f"{data}\n")
+    received_text.config(state=tk.DISABLED)  #wyłączenie edycji
+    received_text.see(tk.END)  #przewijanie
 
 
 def start_receiving_data():
-    root.after(100, receive_data)
+    root.after(100, receive_data) #co 100ms ta funkcja sie uruchomi
 
 
 def close_serial_connection():
     if ser and ser.is_open:
         ser.close()
 
-
-root = tk.Tk()
+root = tk.Tk() #utworzenie okinka
 root.title("Aplikacja do przesyłania i odbierania danych")
 
-label = tk.Label(root, text="Wprowadź dwucyfrową temperaturę (°C):")
+label = tk.Label(root, text="Wprowadź wartość temperatury w zakresie od 12°C do 41°C:")
 label.pack(pady=10)
 
 entry = tk.Entry(root)
 entry.pack(pady=5)
+
+send_button = tk.Button(root, text="Wyślij", command=send_temperature)
+send_button.pack(pady=10)
 
 port_label = tk.Label(root, text="Wybierz port szeregowy:")
 port_label.pack(pady=10)
@@ -72,8 +90,8 @@ ports = ["COM1", "COM2", "COM3", "COM4", "COM5"]
 port_menu = tk.OptionMenu(root, port_var, *ports)
 port_menu.pack(pady=5)
 
-send_button = tk.Button(root, text="Wyślij", command=send_temperature)
-send_button.pack(pady=10)
+connect_with_USART_button = tk.Button(root, text="Połącz", command=connect_with_USART)
+connect_with_USART_button.pack(pady=10)
 
 received_text = tk.Text(root, height=10, state=tk.DISABLED)
 received_text.pack(pady=10)
@@ -81,6 +99,8 @@ received_text.pack(pady=10)
 receive_button = tk.Button(root, text="Odbieraj dane", command=start_receiving_data)
 receive_button.pack(pady=10)
 
+# Zamykanie połączenia szeregowego przed zamknięciem aplikacji
 root.protocol("WM_DELETE_WINDOW", close_serial_connection)
 
+# Uruchomienie aplikacji
 root.mainloop()
